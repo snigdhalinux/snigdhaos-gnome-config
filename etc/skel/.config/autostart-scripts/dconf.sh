927939d6358f785d4639961fca4aa2b6af866ee3
Eshan Roy (Eshanized) <src.eshan@gmail.com>
2023-12-14 13:30:49 +0530

    upd

---
+++ etc/skel/.config/autostart-scripts/dconf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+dconf load / < ~/.config/snigdhaos-dconf.ini
+rm -f ~/.config/snigdhaos-dconf.ini ~/.config/autostart-scripts/dconf.sh &
+ 
+notify-send "Snigdha OS Gnome Config Applied... 🔥"
