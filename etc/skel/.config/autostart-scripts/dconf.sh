5283c8fdf264861b03c7e150681faffd88f9ccae
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-28 20:28:22 +0530

    Update dconf.sh

--- etc/skel/.config/autostart-scripts/dconf.sh
+++ etc/skel/.config/autostart-scripts/dconf.sh
@@ -2,4 +2,4 @@
 dconf load / < ~/.config/snigdhaos-dconf.ini
 rm -f ~/.config/snigdhaos-dconf.ini ~/.config/autostart-scripts/dconf.sh &
  
-notify-send "Snigdha OS Gnome Config 2.0 Applied... 🔥"
+notify-send "Snigdha OS Gnome Config 3.0 Applied... 🔥"
